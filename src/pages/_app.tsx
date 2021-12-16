141f6422a073351959e6b3c8287a938875faf734
Esi're <esiri92@gmail.com>
2021-12-16 10:01:08 +0100

    add moralis react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app';
 import { FC } from 'react';
+import { MoralisProvider } from 'react-moralis';
 import '~/styles/globals.css';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 };
 
 export default MyApp;
