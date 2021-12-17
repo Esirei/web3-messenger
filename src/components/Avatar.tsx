51ec6519e699b442a6514effdb0fe1d81490e753
Esi're <esiri92@gmail.com>
2021-12-17 17:50:45 +0100

    logout confirmation. Remove logout button as avatar can be used to logout.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -9,13 +9,19 @@ interface Props {
 
 const Avatar: FC<Props> = ({ logoutOnPress, username }) => {
   const { user, logout } = useMoralis();
+
+  const confirmLogout = () => {
+    confirm('Are you sure you want to logout?') && logout();
+  };
+
   return (
     <div>
       <Image
+        title={logoutOnPress && 'Logout'}
         className="rounded-full bg-black cursor-pointer hover:opacity-75 duration-200"
         src={`https://avatars.dicebear.com/api/pixel-art/${username ?? user.getUsername()}.svg`}
         layout="fill"
-        onClick={() => logoutOnPress && logout()}
+        onClick={() => logoutOnPress && confirmLogout()}
       />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Avatar from '~/components/Avatar';
 import ChangeUsername from '~/components/ChangeUsername';
 
 const Header: FC = () => {
-  const { user, logout } = useMoralis();
+  const { user } = useMoralis();
   return (
     <div className="sticky top-0 p-5 z-50 bg-black shadow-sm text-pink-500 border-b-2 border-pink-700">
       <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
@@ -27,12 +27,6 @@ const Header: FC = () => {
           <h1 className="text-3xl">Welcome to the metaverse!</h1>
 
           <h2 className="text-5xl font-bold truncate">{user.getUsername()}</h2>
-
-          <button
-            className="p-4 py-2 mt-2 font-bold text-white bg-pink-500 rounded-lg"
-            onClick={logout}>
-            Log out
-          </button>
 
           <ChangeUsername />
         </div>
